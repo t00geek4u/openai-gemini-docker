cc76eda995b21e7bae65556f926efe265ea0219a
t00geek4u <57046062+t00geek4u@users.noreply.github.com>
2024-10-25 01:10:11 -0400

    Add files via upload

---
+++ setup_run.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/PublicAffairs/openai-gemini.git
+git clone https://github.com/t00geek4u/openai-gemini-docker.git
+mv openai-gemini-docker/Dockerfile openai-gemini
+mv openai-gemini-docker/docker-compose.yml openai-gemini
+rm -fr openai-gemini-docker
+cd openai-gemini
+docker compose up -d
+docker logs openai-gemini
